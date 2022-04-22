d0085b687baa37e9cd37c289a97f44dc770679db
bp277 <bosko277@gmail.com>
2022-04-22 14:43:52 +0200

    first commit

---
+++ js/eastern.js
@@ -0,0 +1,189 @@
+const miami = document.querySelector('.miami');
+const atlanta = document.querySelector('.atlanta');
+const philly = document.querySelector('.philly');
+const toronto = document.querySelector('.toronto');
+const bucks = document.querySelector('.bucks');
+const chicago = document.querySelector('.chicago');
+const boston = document.querySelector('.boston');
+const brooklyn = document.querySelector('.brooklyn');
+const secondRoundOneEast = document.querySelector('.e-second-round-one');
+const secondRoundTwoEast = document.querySelector('.e-second-round-two');
+const secondRoundThreeEast = document.querySelector('.e-second-round-three');
+const secondRoundFourEast = document.querySelector('.e-second-round-four');
+const eastFinalsOne = document.querySelector('.e-conf-final-one')
+const eastFinalsTwo = document.querySelector('.e-conf-final-two')
+const finalsTeamEast = document.querySelector('.finals-team-two')
+
+
+
+// EASTERN CONFERENCE
+
+miami.addEventListener('click', () => {
+    secondRoundOneEast.classList.add('miami')
+    secondRoundOneEast.classList.remove('atlanta')
+    eastFinalsOne.classList.remove('atlanta')
+    finalsTeamEast.classList.remove('atlanta')
+})
+atlanta.addEventListener('click', () => {
+    secondRoundOneEast.classList.add('atlanta')
+    secondRoundOneEast.classList.remove('miami')
+    eastFinalsOne.classList.remove('miami')
+    finalsTeamEast.classList.remove('miami')
+})
+philly.addEventListener('click', () => {
+    secondRoundTwoEast.classList.add('philly')
+    secondRoundTwoEast.classList.remove('toronto')
+    eastFinalsOne.classList.remove('toronto')
+    finalsTeamEast.classList.remove('toronto')
+})
+toronto.addEventListener('click', () => {
+    secondRoundTwoEast.classList.add('toronto')
+    secondRoundTwoEast.classList.remove('philly')
+    eastFinalsOne.classList.remove('philly')
+    finalsTeamEast.classList.remove('philly')
+})
+bucks.addEventListener('click', () => {
+    secondRoundThreeEast.classList.add('bucks')
+    secondRoundThreeEast.classList.remove('chicago')
+    eastFinalsTwo.classList.remove('chicago')
+    finalsTeamEast.classList.remove('chicago')
+})
+chicago.addEventListener('click', () => {
+    secondRoundThreeEast.classList.add('chicago')
+    secondRoundThreeEast.classList.remove('bucks')
+    eastFinalsTwo.classList.remove('bucks')
+    finalsTeamEast.classList.remove('bucks')
+    
+})
+boston.addEventListener('click', () => {
+    secondRoundFourEast.classList.add('boston')
+    secondRoundFourEast.classList.remove('brooklyn')
+    eastFinalsTwo.classList.remove('brooklyn')
+    finalsTeamEast.classList.remove('brooklyn')
+})
+brooklyn.addEventListener('click', () => {
+    secondRoundFourEast.classList.add('brooklyn')
+    secondRoundFourEast.classList.remove('boston')
+    eastFinalsTwo.classList.remove('boston')
+    finalsTeamEast.classList.remove('boston')
+})
+secondRoundOneEast.addEventListener('click', () => {
+    if (secondRoundOneEast.classList.contains('miami')) {
+        eastFinalsOne.classList.add('miami')
+    } else if (secondRoundOneEast.classList.contains('atlanta')) {
+        eastFinalsOne.classList.add('atlanta')
+    }
+    eastFinalsOne.classList.remove('toronto')
+    finalsTeamEast.classList.remove('toronto')
+    eastFinalsOne.classList.remove('philly')
+    finalsTeamEast.classList.remove('philly')
+})
+
+secondRoundTwoEast.addEventListener('click', () => {
+    eastFinalsOne.classList.remove('miami')
+    finalsTeamEast.classList.remove('miami')
+    eastFinalsOne.classList.remove('atlanta')
+    finalsTeamEast.classList.remove('atlanta')
+    if (secondRoundTwoEast.classList.contains('philly')) {
+        eastFinalsOne.classList.add('philly')
+    } else if (secondRoundTwoEast.classList.contains('toronto')) {
+        eastFinalsOne.classList.add('toronto')
+    }
+})
+secondRoundThreeEast.addEventListener('click', () => {
+    eastFinalsTwo.classList.remove('boston')
+    finalsTeamEast.classList.remove('boston')
+    eastFinalsTwo.classList.remove('brooklyn')
+    finalsTeamEast.classList.remove('brooklyn')
+    if (secondRoundThreeEast.classList.contains('bucks')) {
+        eastFinalsTwo.classList.add('bucks')
+    } else if (secondRoundThreeEast.classList.contains('chicago')) {
+        eastFinalsTwo.classList.add('chicago')
+    }
+    eastFinalsOne.classList('')
+    eastFinalsTwo.classList('')
+    finalsTeamEast.classList('')
+})
+secondRoundFourEast.addEventListener('click', () => {
+    if (secondRoundFourEast.classList.contains('boston')) {
+        eastFinalsTwo.classList.add('boston')
+    } else if (secondRoundFourEast.classList.contains('brooklyn')) {
+        eastFinalsTwo.classList.add('brooklyn')
+    }
+    eastFinalsTwo.classList.remove('bucks')
+    finalsTeamEast.classList.remove('bucks')
+    eastFinalsTwo.classList.remove('chicago')
+    finalsTeamEast.classList.remove('chicago')
+})
+
+eastFinalsOne.addEventListener('click', () => {
+    if (eastFinalsOne.classList.contains('miami')){
+        finalsTeamEast.classList.add('miami')
+        finalsTeamEast.classList.remove('bucks', 'chicago', 'boston', 'brooklyn')
+    } else if (eastFinalsOne.classList.contains('atlanta')) {
+        finalsTeamEast.classList.add('atlanta')
+        finalsTeamEast.classList.remove('bucks', 'chicago', 'boston', 'brooklyn')
+    } else if (eastFinalsOne.classList.contains('philly')) {
+        finalsTeamEast.classList.add('philly')
+        finalsTeamEast.classList.remove('bucks', 'chicago', 'boston', 'brooklyn')
+    } else if (eastFinalsOne.classList.contains('toronto')) {
+        finalsTeamEast.classList.add('toronto')
+        finalsTeamEast.classList.remove('bucks', 'chicago', 'boston', 'brooklyn')
+    }})
+    
+    eastFinalsTwo.addEventListener('click', () => {
+    if (eastFinalsTwo.classList.contains('bucks')){
+        finalsTeamEast.classList.add('bucks')
+        finalsTeamEast.classList.remove('miami', 'atlanta', 'philly', 'toronto')
+    } else if (eastFinalsTwo.classList.contains('chicago')) {
+        finalsTeamEast.classList.add('chicago')
+        finalsTeamEast.classList.remove('miami', 'atlanta', 'philly', 'toronto')
+    } else if (eastFinalsTwo.classList.contains('boston')) {
+        finalsTeamEast.classList.add('boston')
+        finalsTeamEast.classList.remove('miami', 'atlanta', 'philly', 'toronto')
+    } else if (eastFinalsTwo.classList.contains('brooklyn')) {
+        finalsTeamEast.classList.add('brooklyn')
+        finalsTeamEast.classList.remove('miami', 'atlanta', 'philly', 'toronto')
+    }})
+    
+    finalsTeamEast.addEventListener('click', () => {
+        message.classList.add('flex')
+        startConfetti()
+        if (finalsTeamEast.classList.contains('miami')) {
+            winner.textContent = 'Miami Heat'
+        }
+        if (finalsTeamEast.classList.contains('atlanta')) {
+            winner.textContent = 'Atlanta Hawks'
+        }
+        if (finalsTeamEast.classList.contains('philly')) {
+            winner.textContent = 'Philadelphia 76ers'
+        }
+        if (finalsTeamEast.classList.contains('toronto')) {
+            winner.textContent = 'Toronto Raptors'
+        }
+        if (finalsTeamEast.classList.contains('bucks')) {
+            winner.textContent = 'Milwaukee Bucks'
+        }
+        if (finalsTeamEast.classList.contains('chicago')) {
+            winner.textContent = 'Chicago Bulls'
+        }
+        if (finalsTeamEast.classList.contains('boston')) {
+            winner.textContent = 'Boston Celtics'
+        }
+        if (finalsTeamEast.classList.contains('brooklyn')) {
+            winner.textContent = 'Brooklyn Nets'
+        }
+    })
+
+    //RESET
+
+    reset.addEventListener('click', () => {
+        secondRoundOneEast.classList = ('square')
+        secondRoundTwoEast.classList = ('square')
+        secondRoundThreeEast.classList = ('square')
+        secondRoundFourEast.classList = ('square')
+        eastFinalsOne.classList = ('square')
+        eastFinalsTwo.classList = ('square')
+        finalsTeamEast.classList = ('square-f')
+        stopConfetti()
+    })
---
+++ js/western.js
@@ -0,0 +1,198 @@
+const phoenix = document.querySelector('.phoenix');
+const nop = document.querySelector('.nop');
+const dallas = document.querySelector('.dallas');
+const utah = document.querySelector('.utah');
+const gsw = document.querySelector('.gsw');
+const denver = document.querySelector('.denver');
+const memphis = document.querySelector('.memphis');
+const minnesota = document.querySelector('.minnesota');
+const secondRoundOne = document.querySelector('.w-second-round-one');
+const secondRoundTwo = document.querySelector('.w-second-round-two');
+const secondRoundThree = document.querySelector('.w-second-round-three');
+const secondRoundFour = document.querySelector('.w-second-round-four');
+const westFinalsOne = document.querySelector('.w-conf-final-one')
+const westFinalsTwo = document.querySelector('.w-conf-final-two')
+const finalsTeamWest = document.querySelector('.finals-team-one')
+
+const message = document.querySelector('.message')
+const winner = document.querySelector('.winner-team')
+
+
+
+// WESTERN CONFERENCE
+
+phoenix.addEventListener('click', () => {
+    secondRoundOne.classList.add('phoenix')
+    secondRoundOne.classList.remove('nop')
+    westFinalsOne.classList.remove('nop')
+    finalsTeamWest.classList.remove('nop')
+})
+nop.addEventListener('click', () => {
+    secondRoundOne.classList.add('nop')
+    secondRoundOne.classList.remove('phoenix')
+    westFinalsOne.classList.remove('phoenix')
+    finalsTeamWest.classList.remove('phoenix')
+})
+
+
+dallas.addEventListener('click', () => {
+    secondRoundTwo.classList.add('dallas')
+    secondRoundTwo.classList.remove('utah')
+    westFinalsOne.classList.remove('utah')
+    finalsTeamWest.classList.remove('utah')
+})
+utah.addEventListener('click', () => {
+    secondRoundTwo.classList.add('utah')
+    secondRoundTwo.classList.remove('dallas')
+    westFinalsOne.classList.remove('dallas')
+    finalsTeamWest.classList.remove('dallas')
+})
+gsw.addEventListener('click', () => {
+    secondRoundThree.classList.add('gsw')
+    secondRoundThree.classList.remove('denver')
+    westFinalsTwo.classList.remove('denver')
+    finalsTeamWest.classList.remove('denver')
+})
+denver.addEventListener('click', () => {
+    secondRoundThree.classList.add('denver')
+    secondRoundThree.classList.remove('gsw')
+    westFinalsTwo.classList.remove('gsw')
+    finalsTeamWest.classList.remove('gsw')
+    
+})
+
+
+memphis.addEventListener('click', () => {
+    secondRoundFour.classList.add('memphis')
+    secondRoundFour.classList.remove('minnesota')
+    westFinalsTwo.classList.remove('minnesota')
+    finalsTeamWest.classList.remove('minnesota')
+})
+minnesota.addEventListener('click', () => {
+    secondRoundFour.classList.add('minnesota')
+    secondRoundFour.classList.remove('memphis')
+    westFinalsTwo.classList.remove('memphis')
+    finalsTeamWest.classList.remove('memphis')
+})
+secondRoundOne.addEventListener('click', () => {
+    if (secondRoundOne.classList.contains('phoenix')) {
+        westFinalsOne.classList.add('phoenix')
+    } else if (secondRoundOne.classList.contains('nop')) {
+        westFinalsOne.classList.add('nop')
+    }
+    westFinalsOne.classList.remove('utah')
+    finalsTeamWest.classList.remove('utah')
+    westFinalsOne.classList.remove('dallas')
+    finalsTeamWest.classList.remove('dallas')
+})
+
+secondRoundTwo.addEventListener('click', () => {
+    westFinalsOne.classList.remove('phoenix')
+    finalsTeamWest.classList.remove('phoenix')
+    westFinalsOne.classList.remove('nop')
+    finalsTeamWest.classList.remove('nop')
+    if (secondRoundTwo.classList.contains('dallas')) {
+        westFinalsOne.classList.add('dallas')
+    } else if (secondRoundTwo.classList.contains('utah')) {
+        westFinalsOne.classList.add('utah')
+    }
+})
+secondRoundThree.addEventListener('click', () => {
+    if (secondRoundThree.classList.contains('gsw')) {
+        westFinalsTwo.classList.add('gsw')
+    } else if (secondRoundThree.classList.contains('denver')) {
+        westFinalsTwo.classList.add('denver')
+    }
+    westFinalsTwo.classList.remove('memphis')
+    finalsTeamWest.classList.remove('memphis')
+    westFinalsTwo.classList.remove('minnesota')
+    finalsTeamWest.classList.remove('minnesota')
+})
+secondRoundFour.addEventListener('click', () => {
+    if (secondRoundFour.classList.contains('memphis')) {
+        westFinalsTwo.classList.add('memphis')
+    } else if (secondRoundFour.classList.contains('minnesota')) {
+        westFinalsTwo.classList.add('minnesota')
+    }
+    westFinalsTwo.classList.remove('gsw')
+    finalsTeamWest.classList.remove('gsw')
+    westFinalsTwo.classList.remove('denver')
+    finalsTeamWest.classList.remove('denver')
+})
+
+westFinalsOne.addEventListener('click', () => {
+    if (westFinalsOne.classList.contains('phoenix')){
+        finalsTeamWest.classList.add('phoenix')
+        finalsTeamWest.classList.remove('gsw', 'denver', 'memphis', 'minnesota')
+    } else if (westFinalsOne.classList.contains('nop')) {
+        finalsTeamWest.classList.add('nop')
+        finalsTeamWest.classList.remove('gsw', 'denver', 'memphis', 'minnesota')
+    } else if (westFinalsOne.classList.contains('dallas')) {
+        finalsTeamWest.classList.add('dallas')
+        finalsTeamWest.classList.remove('gsw', 'denver', 'memphis', 'minnesota')
+    } else if (westFinalsOne.classList.contains('utah')) {
+        finalsTeamWest.classList.add('utah')
+        finalsTeamWest.classList.remove('gsw', 'denver', 'memphis', 'minnesota')
+    }})
+    
+    westFinalsTwo.addEventListener('click', () => {
+        if (westFinalsTwo.classList.contains('gsw')){
+            finalsTeamWest.classList.add('gsw')
+            finalsTeamWest.classList.remove('phoenix', 'nop', 'dallas', 'utah')
+        } else if (westFinalsTwo.classList.contains('denver')) {
+            finalsTeamWest.classList.add('denver')
+            finalsTeamWest.classList.remove('phoenix', 'nop', 'dallas', 'utah')
+        } else if (westFinalsTwo.classList.contains('memphis')) {
+            finalsTeamWest.classList.add('memphis')
+            finalsTeamWest.classList.remove('phoenix', 'nop', 'dallas', 'utah')
+        } else if (westFinalsTwo.classList.contains('minnesota')) {
+            finalsTeamWest.classList.add('minnesota')
+            finalsTeamWest.classList.remove('phoenix', 'nop', 'dallas', 'utah')
+        }})
+
+        finalsTeamWest.addEventListener('click', () => {
+            message.classList.add('flex')
+            startConfetti();
+            if (finalsTeamWest.classList.contains('phoenix')) {
+                winner.textContent = 'Phoenix Suns'
+            }
+            if (finalsTeamWest.classList.contains('nop')) {
+                winner.textContent = 'New Orleans Pelicans'
+            }
+            if (finalsTeamWest.classList.contains('dallas')) {
+                winner.textContent = 'Dallas Mavericks'
+            }
+            if (finalsTeamWest.classList.contains('utah')) {
+                winner.textContent = 'Utah Jazz'
+            }
+            if (finalsTeamWest.classList.contains('gsw')) {
+                winner.textContent = 'Golden State Warriors'
+            }
+            if (finalsTeamWest.classList.contains('denver')) {
+                winner.textContent = 'Denver Nuggets'
+            }
+            if (finalsTeamWest.classList.contains('memphis')) {
+                winner.textContent = 'Memphis Grizzlies'
+            }
+            if (finalsTeamWest.classList.contains('minnesota')) {
+                winner.textContent = 'Minnesota Timberwolfes'
+            }
+
+        })
+
+        //RESET
+
+        const reset = document.querySelector('.reset-btn')
+
+        reset.addEventListener('click', () => {
+            secondRoundOne.classList = ('square')
+            secondRoundTwo.classList = ('square')
+            secondRoundThree.classList = ('square')
+            secondRoundFour.classList = ('square')
+            westFinalsOne.classList = ('square')
+            westFinalsTwo.classList = ('square')
+            finalsTeamWest.classList = ('square-f')
+            message.classList.remove('flex')
+            winner.textContent = ''
+            stopConfetti()
+        })
